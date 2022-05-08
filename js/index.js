0649576925e0cc929e117e1802964166b56f951b
pdcourses <freshitpt@gmail.com>
2022-05-08 10:49:21 +0300

    простые операции с переменными

--- js/index.js
+++ js/index.js
@@ -6,12 +6,14 @@ console.log("Hello ","Java Script", "!!!!!");
 //******************* */
 
 // переменые style camelCase;
+
+//обьявление переменных
 let x;
 let y;
 let userName;
 let userColorAvatarBG; 
 let a, b, c;
-
+// инициализация переменных
 x = 5;
 y = -100;
 a = 3.14;
@@ -23,3 +25,74 @@ console.log(x);
 console.log(y,a,b);
 console.log("User name is ",userName);
 console.log('A = ',a,' and B = ', b);
+
+// обьявление с инициализацией
+let d = 200;
+let name1 = "Tom Fox";
+console.log(name1);
+
+// Что мы можем положить в (контейнер) переменную? ТИПЫ
+let x2;
+x2 = 100;
+console.log(x2);
+x2 = -3;
+console.log(x2);
+x2 = "hello";
+console.log(x2);
+x2 = 'vasya';
+console.log(x2);
+x2 = true; // логический тип true , false
+console.log(x2);
+
+// мы можем ложить контейнеры один в один
+console.log('x=',x);
+console.log('y=',y);
+x = y;
+console.log('x=',x);
+console.log('y=',y);
+
+// операции
+x = 10;
+
+console.log('x=',x);
+x = x + 5;
+console.log('x=',x);
+x = x - 5;
+console.log('x=',x);
+x = x * 5;
+console.log('x=',x);
+x = x / 5;
+console.log('x=',x);
+
+y = 2;
+x = 10;
+
+console.log('x=',x);
+x = x + y;
+console.log('x=',x);
+x = x - y;
+console.log('x=',x);
+x = x * y;
+console.log('x=',x);
+x = x / y;
+console.log('x=',x);
+
+let a1 =10;
+let a2 = 0, a3 = "Julia";
+let a4 = false, a5 = -2;
+
+let result;
+result = a1 + a5;
+result = a1 - a5;
+result = a1 * a5;
+result = a1 / a5;
+console.log(result);
+
+result = result * 2;
+console.log(result);
+
+result = 500 - result;
+console.log(result);
+
+
+

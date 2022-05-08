8b7fc40d720e988e1e9c626824774520cd68c850
pdcourses <freshitpt@gmail.com>
2022-05-08 10:17:12 +0300

    console.log examples

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//alert("Hello, World!");
+
+console.log("Hello, Vasya!!!");
+console.log("Hello ","Java Script", "!!!!!");
+
+//******************* */
+
+// переменые style camelCase;
+let x;
+let y;
+let userName;
+let userColorAvatarBG; 
+let a, b, c;
+
+x = 5;
+y = -100;
+a = 3.14;
+b = 0.00005;
+userName = "Vasya";
+userColorAvatarBG = "red";
+
+console.log(x);
+console.log(y,a,b);
+console.log("User name is ",userName);
+console.log('A = ',a,' and B = ', b);

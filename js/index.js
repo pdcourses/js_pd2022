df84e18390c896341edf18c2d36bc3b0598c0d87
pdcourses <freshitpt@gmail.com>
2022-05-22 14:07:14 +0300

    виды функций

--- js/index.js
+++ js/index.js
@@ -1,159 +1,58 @@
 /*
-Область видимость
-Лексическая область видимости. Лексическое окружение
-*/
+Виды функции (3 вида):
 
-/*
-function f(a,b){
-    var x=5;
+1. function definition
+function funName(param1, param2, param3, ...){
+    return result;
 }
-*/
 
-//******************** */
-/*
-let sum=0;
-//console.log('i=',i); error!
-for(var i=0; i<10; i++){
-    sum += i;
-    console.log(i, sum);
+2. function expression
+
+const funName = function(param1, param2, param3, ...){
+    return result;
 }
-console.log(sum);
-//console.log('i=',i); error!
 
 
-console.log('i=',i);
-*/
+3. arrow functions
 
-/*
-if(true){
-    const test=100;
-    console.log(test);
+const funName = (param1, param2, param3, ...) => {
+    return result;
 }
-*/
-/*
-console.log(test);
-if(true){
-    var test=200;
-    console.log(test);
-}
-*/
 
-/*
-// поднятие переменных и замыкание
-if (true) {
-  console.log(test);
-  if (true) {
-    console.log(test);
-    if (true) {
-      console.log(test);
-      if (true) {
-        console.log(test);
-        if (true) {
-          console.log(test);
-          var test = 100;
-          console.log(test);
-        }
-        test=200;
-        console.log(test);
-      }
-      test=300;
-      console.log(test);
+
+*/ 
+
+function sum1(a){
+    let res = 0;
+    for(let i=0; i<a.length; i++){
+        res += a[i];
     }
-    test=400;
-    console.log(test);
-  }
-  test=500;
-  console.log(test);
+    return res;
 }
 
-console.log(this); // доспуп к глобальному обьекту window
-
-console.log(this.test);
-
-*/
-/*
-
-// строгий режим
-"use strict";
-function f(){
-    let test=10;
-    return test;
-}
-
-console.log(f());
-// ***************************************
-// глобальная область
-let x = 100;
-
-function fun1(){
-    console.log('x=',x);
-}
-
-function fun2(){
-    let x = 200;
-    console.log('x=',x);
-}
-
-function fun3(){
-    x = 300;
-    console.log('x=',x);
-}
-
-function fun4(){
-    let a = 10, b=20;
-    return (a*b  + 100 - 4*a + 5 )/x;
-}
-
-
-console.log(x); //100
-fun1();  //100
-
-fun2(); // 200
-console.log(x); //??  100 glob x
-
-fun3(); // 300
-console.log(x); //?? 300  glob x
-
-
-console.log(fun4()); // x=300
-console.log(x); //?? x=100
-
-//**************** */
-/*
-let name = "John";
-
-function sayHi() {
-  alert("Hi, " + name);
-}
-
-name = "Pete";
-
-sayHi(); // что будет показано: "John" или "Pete"?
-*/
-//************** */
-console.log('************************');
-
-function fun(){
-    let test = 10;
-    return function(){
-        return ++test;
+const sum2 = function(a){
+    let res = 0;
+    for(let i=0; i<a.length; i++){
+        res += a[i];
     }
+    return res;
 }
 
-let res = fun();
-console.log(res());
-console.log(res());
-console.log(res());
-console.log(res());
-console.log(res());
-console.log(res());
-console.log(res());
-console.log('************************');
-let res2 = fun();
-console.log(res2());
-console.log(res2());
-console.log(res2());
-console.log(res2());
-console.log(res2());
-console.log(res2());
-console.log(res2());
+const sum3 = (a) => {
+    let res = 0;
+    for(let i=0; i<a.length; i++){
+        res += a[i];
+    }
+    return res;
+}
+
+const funSumXY = (x,y) => {
+    return x+y;
+}
+// сокращенная записать
+const funSumXY = (x,y) => x+y;
+
+
+
+
+

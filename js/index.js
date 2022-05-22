37efe3cb7d0dd8e83c6f02bfcd3ede52aadf120a
pdcourses <freshitpt@gmail.com>
2022-05-22 13:33:22 +0300

    переменные и область видимости

--- js/index.js
+++ js/index.js
@@ -1,99 +1,124 @@
 /*
-Фукнцию которая считает сумму отрицательных элементов массива
-
-она должна использовать (вызвать) 2 функции
-1 находит и возвращает отрицательные элементы массива
-2 считает сумму массива
+Область видимость
+Лексическая область видимости. Лексическое окружение
 */
-
-function sumArray(a){
-    let sum = 0;
-    for(let i=0; i<a.lenght; i++ ){
-        sum += a[i];
-    }
-    return sum;
-}
-
-function returnArrayElsBelowZero(a){
-    // вернет набор элементов меньше нуля
-    let result = [], j, i;
-    for(i=0, j=0; i < a.lenght; i++ ){
-        if(a[i] < 0 ){
-            result[j] = a[i];
-            j++;
-        }
-    }
-    return result;
-}
-
-
-function sumElsBelowZero(array){
-    /*
-    let arrayEls = returnArrayElsBelowZero(array);
-    let res = sumArray(arrayEls);
-    return res;
-    */
-   return sumArray(returnArrayElsBelowZero(array));
-}
-
-console.log(sumElsBelowZero([1,2,-1,-5,0,3,-4,6]));
-
-//***************** */
-//Задание если число четное то умножить его на 2 (в виде функций)
-
-function isEvenNumber(number){
-    if(number%2 == 0) {return true};
-    //else {return false};
-    return false;
-}
-
-function task(number){
-    //if(isEvenNumber(number) == true) {
-    if(isEvenNumber(number)){
-        number = number *2;
-    }
-    return number;
-}
-
-let res;
-res = task(5);
-console.log(res);
-res = task(6);
-console.log(res);
-
-
-/*написать функцию которая вернет максимальный элемент массива*/
-function maxElOfArray(array){
-    let max = array[0];
-    for(let i=0; i<array.lenght; i++){
-        if(a[i] > max){
-            max = a[i];
-        }
-    }
-    return max;
-} 
-
-function minElOfArray(array){
-    let min = array[0];
-    for(let i=0; i<array.lenght; i++){
-        if(a[i] < min){
-            min = a[i];
-        }
-    }
-    return min;
-}
 
 /*
-[1,2,-1,-5,0,3,-4,6]
-max = 1
-i=0 1 max = 1
-i=1 2 2>max yes! max =2
-i=2 -1   -1> max -
-i=3 -5    -5>max  -
-i=4 0     0> max -
-i=5 3    3>max  ++++ max=3
-i=6 -4   -4>max (3) -
-i=7 6   6>max(3)  +++ max = 6
-max = 6
+function f(a,b){
+    var x=5;
+}
+*/
+
+//******************** */
+/*
+let sum=0;
+//console.log('i=',i); error!
+for(var i=0; i<10; i++){
+    sum += i;
+    console.log(i, sum);
+}
+console.log(sum);
+//console.log('i=',i); error!
+
+
+console.log('i=',i);
+*/
+
+/*
+if(true){
+    const test=100;
+    console.log(test);
+}
+*/
+/*
+console.log(test);
+if(true){
+    var test=200;
+    console.log(test);
+}
+*/
+
+/*
+// поднятие переменных и замыкание
+if (true) {
+  console.log(test);
+  if (true) {
+    console.log(test);
+    if (true) {
+      console.log(test);
+      if (true) {
+        console.log(test);
+        if (true) {
+          console.log(test);
+          var test = 100;
+          console.log(test);
+        }
+        test=200;
+        console.log(test);
+      }
+      test=300;
+      console.log(test);
+    }
+    test=400;
+    console.log(test);
+  }
+  test=500;
+  console.log(test);
+}
+
+console.log(this); // доспуп к глобальному обьекту window
+
+console.log(this.test);
 
 */
+
+
+// строгий режим
+"use strict";
+function f(){
+    let test=10;
+    return test;
+}
+
+console.log(f());
+// ***************************************
+// глобальная область
+let x = 100;
+
+function fun1(){
+    console.log('x=',x);
+}
+
+function fun2(){
+    let x = 200;
+    console.log('x=',x);
+}
+
+function fun3(){
+    x = 300;
+    console.log('x=',x);
+}
+
+function fun4(){
+    let a = 10, b=20;
+    return (a*b  + 100 - 4*a + 5 )/x;
+}
+
+
+console.log(x); //100
+fun1();  //100
+
+fun2(); // 200
+console.log(x); //??  100 glob x
+
+fun3(); // 300
+console.log(x); //?? 300  glob x
+
+
+console.log(fun4()); // x=300
+console.log(x); //?? x=100
+
+
+
+
+

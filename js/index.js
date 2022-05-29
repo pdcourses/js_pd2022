3fd36dd27c5cd9a6dedc40968b6914405b1f228f
pdcourses <freshitpt@gmail.com>
2022-05-29 10:58:15 +0300

    task1

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@
 
 // a = [10,5,6,7,0,1,1,1,0,12]
 
+/*
 function task1(a){
     let sum = 0;
     for(let i=0; i<arr.length; i++){
@@ -58,3 +59,49 @@ function task4(a){
     }
     return res;
 }
+
+*/
+
+/*
+//1-1
+let a = [4,0,8,10,1,0,5,7,5,0,4,6];
+
+//нечетное?
+function isOdd(value) {
+    return value%2 != 0;
+  }
+  
+let res1 = a.filter(isOdd); //оставлю только нечетные 
+
+
+function sum(accum, val) {
+    return accum, val;
+}
+
+let res2 = res1.reduce(sum);
+*/
+
+/*
+//1-2
+let resOdd = a.filter(isOdd = (value) => value%2 == 1);
+let resSum = resOdd.reduce(sumNum = (a, b) => a + b)
+console.log(resSum);
+*/
+
+
+//1-3
+//let sum = (a) => a.filter((el, i) => (el%2)).reduce((accum, val) => accum + val);
+
+//1-4
+
+let sum = (a) => {
+    let res = 0;
+    a.forEach((el) => {
+        if(el%2 != 0) res += el;
+    });
+    return res;
+};
+
+// нечетный оставляю, а любой четный превращаю в ноль, а потом сумма с помощью reduce
+let sum = (a) => a.forEach((el) => (el%2 != 0) ? el : 0).reduce((accum, val) => accum + val);
+

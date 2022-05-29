d08483cc0a07294f1a2b981335e73042605aa63b
pdcourses <freshitpt@gmail.com>
2022-05-29 10:34:12 +0300

    practice array. велосипеды

--- js/index.js
+++ js/index.js
@@ -1,88 +1,60 @@
-let a1 = [18,20, 22, 25, 18, 20, 20];
-let a2 = [18, 14, 22, 25, 18, 20, 20];
-/* EVERY */
-function isEveryAdult1(arr){
-    for(i=0; i<arr.length; i++){
-        if(arr[i]<18) return false;
+/*
+1 Подсчитать сумму нечетных эелементов массива
+2 Вывести индексы нулевых элементов массива
+3 дано 2 массива. соединить и преобразовать в строку
+4 отфильтровать масив, удалив все нули
+*/
+
+// a = [10,5,6,7,0,1,1,1,0,12]
+
+function task1(a){
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+        if(a[i]%2 != 0){
+            sum = sum + a[i];
+        }        
     }
-    return true;
+    return sum;
 }
-console.log(isEveryAdult1(a1));
-console.log(isEveryAdult1(a2));
-//2
-function isEveryAdult2(el, i, arr) {
-    return el >= 18;
-  }
-console.log(a1.every(isEveryAdult2));
-console.log(a2.every(isEveryAdult2)); 
-/*SOME */
-//1
-function isSomeAdult1(arr){
-    for(i=0; i<arr.length; i++){
-        if(arr[i]>=18) return true;
+
+function task2(a){
+    let res=[],i,j;
+    for(i=0,j=0; i<a.length; i++){
+        if(a[i] == 0){
+            res[j] = a[i];
+            j++;
+        }
     }
-    return false;
-}
-console.log(isSomeAdult1(a1));
-console.log(isSomeAdult1(a2));
-//2
-function isSomeAdult2(el) {
-    return el >= 18;
-  }
-console.log(a1.some(isSomeAdult2));
-console.log(a2.some(isSomeAdult2)); 
-
-//*************************** */
-
-/*findIndex  find*/
-// число превысит 10
-function NumBiggerThan10(element, index, array){
-    if (element > 10) return true;
-    return false;
-}
-
-let a = [4,1,0,-5,11,20,5,10];
-console.log(a.findIndex(NumBiggerThan10));
-/* find */ 
-console.log(a.find(NumBiggerThan10));
-
-/*includes*/ 
-console.log(a.includes(11));
-
-
-/* forEach */
-
-const array1 = [1, 2, 3, 4];
-
-// 0 + 1 + 2 + 3 + 4
-const initialValue = 0;
-function f(previousValue, currentValue){
-    return previousValue + currentValue;
-}
-const total = array1.reduce(f,initialValue);
-
-console.log(totall);
-
-
-// hw f(-1,5) -000001   f(1,5) 000001
-
-function addZerosForNum(num, countZeros) {
-    let i, res = [];
-    if(num<0){res[0]='-';}
-    else{res[0]='+';}
-    for(i=1; i<=countZeros; i++){
-        res[i]+='0';
-    }
-    res += Math.abs(num);
     return res;
 }
 
-// массив слов. найти слова больше 4 символов
+function task3(a1, a2){
+    let res = [];
+    res = a1.slice(); // last index a1.length-1
+    for(i=0, j=a1.length; i<a2.length; i++, j++){
+        res[j]=a2[i];
+    }
+    return res.toString();
+}
 
+function task3_2(a1, a2){
+    let s1,s2, res;
+    s1 = a1.toString();
+    s2 = s2.toString();
+    res = s1 + s2;
+    return res;
+}
 
-const words = ['hello', 'Vasya', 'Oh', 'fox', 'JS', 'array'];
-
-
-
-
-
+// отфильтровать масив, удалив все нули
+// итоговый массив без нулей!
+// собрать массив только тех кто не ноль
+function task4(a){
+    let res = [] , i, j;
+    for(i=0, j=0; i<a.length; i++){
+        if(a[i] !=0 ){
+            res[j] = a[i];
+            j++;
+        }
+    }
+    return res;
+}

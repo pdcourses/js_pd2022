3df9ab92e99d7508c76f1622677c94acdaf292d3
pdcourses <freshitpt@gmail.com>
2022-06-11 11:46:24 +0300

    private fileds

--- js/index.js
+++ js/index.js
@@ -1,66 +1,80 @@
-let container   // !!!! no type!
-const RED_COLOR = "#ff0000"
-
- набор [{login: 'login1', pswd: 'qwerty'}, {}, {}]
-push()
-
------------
-simply
-
-complex object {}
-name.value  name.innerObject.value
---------------
-function addTwoNumbersFunction(number1, number2){
-       return number1 + number2;
-}
-
-const f = () => {}
-
-class addTwoNumbers{
-   constructor(number1, number2){
-       this.number1 = number1
-       this.number2 = number2
-   }
-   add(){
-       // внутреннее свойство
-      this.sum  = this.number1 + this.number2;
-      return this.sum;
-   } 
-   add2(){
-      return (this.number1 + this.number2);
-   }
-     
+/*
+class User {
+    constructor(login, password){
+        this.login = login;
+        this.password = password;
+    }
+    test(){
+        console.log(this.login, ': ', this.password);
+    }
+    updatePassword(newPassword){
+        this.password = newPassword;
+    }
 };
 
-let obj1 = addTwoNumbers(5,12);
-// obj1.number1 = 5, obj1.number2 = 12
-// let obj1 = {number1: 5, number2: 12};
+const obj1 = new User('vasya', 'qwerty');
+obj1.test();
+obj1.updatePassword('123dkrke1!-');
+obj1.test();
+*/
 
-let res1 = obj1.add();  // {number1: 5, number2: 12, sum: 17}
+// Обновить поле или прочитать значение свойства
+// get - получить , set - установить или обновить
+/*
+class User {
+    constructor(login, password){
+        this.login = login;
+        this.password = password;
+    }
+    test(){
+        console.log(this.login, ': ', this.password);
+    }
+    set password(value){
+        this.password = value;
+    }
+    set login(value){
+        this.login = value;
+    }
+    get password(){
+        return this.password;
+    }
+    get login(){
+        return this.login;
+    }
+};
+*/
+//id login pasword fullname ...
+// 1001 'login1' 'qwertt' ........ 
+// 1002 'vasya' 'qwertt' ........
 
 
-***************************
 
-class Calc{
-   constructor(number1, number2){
-       this.number1 = number1;
-       this.number2 = number2;
-       this.result = 0;
-   }
-   add(){ this.result  = this.number1 + this.number2; } 
-   sub(){ this.result  = this.number1 - this.number2;}
-   divi(){ this.result  = this.number1 / this.number2; } 
-   mult(){ this.result  = this.number1 * this.number2; }
-   answer() { console.log('Answer is ', this.result);}    
+
+class User {
+    constructor(login, password, fullname = 'uknown'){
+        this.fullname = fullname;
+        this._login = login;
+        this._password = password;
+    }
+    test(){
+        console.log(this._login, ': ', this._password);
+    }
+    set password(password){
+        this._password = password;
+    }
+    set login(login){
+        this._login = login;
+    }
+    get password(){
+        return this._password;
+    }
+    get login(){
+        return this._login;
+    }
 };
 
-let test = new Calc(10,20);
-test.add();
-test.answer();
-
-
-
-
-
-
+let obj2 = new User('vasyalogin123', 'qwerty'); 
+obj2.password = 'admin'; 
+console.log(obj2.password);
+obj2.test();
 

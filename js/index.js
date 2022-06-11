08e1d12432ea8dea12f0dabc4dc972b1a19ed3ba
pdcourses <freshitpt@gmail.com>
2022-06-11 10:40:42 +0300

    class. add inner fields

--- js/index.js
+++ js/index.js
@@ -1,68 +1,66 @@
-/*CLASS*/
+let container   // !!!! no type!
+const RED_COLOR = "#ff0000"
 
-// ООП
+ набор [{login: 'login1', pswd: 'qwerty'}, {}, {}]
+push()
 
-/*
-фундамент - объект
+-----------
+simply
 
-class - класс для обьекта (не класс правила CSS)
-сложный тип данных, то что описывает объект, набор свойств
-очень похожа на шаблон
-родитель для объектов
-"паска"
-"шаблон"
-"станок"
-*/
+complex object {}
+name.value  name.innerObject.value
+--------------
+function addTwoNumbersFunction(number1, number2){
+       return number1 + number2;
+}
 
-//{login: '', password: ''}
-class User{
-    //constructor - функция которая описывает создание будущих объектов
-    // this - указатель на текущий объект
-    // в качетсве параметра - примитив (число, строка..) [] {}
-    constructor(loginValue, passwordValue){
-        this.login = loginValue;
-        this.password = passwordValue;
-    }
-    outObject(){
-        console.log(this);
-    }
+const f = () => {}
+
+class addTwoNumbers{
+   constructor(number1, number2){
+       this.number1 = number1
+       this.number2 = number2
+   }
+   add(){
+       // внутреннее свойство
+      this.sum  = this.number1 + this.number2;
+      return this.sum;
+   } 
+   add2(){
+      return (this.number1 + this.number2);
+   }
+     
 };
 
-// new - оператор для создания нового объекта
-const user1 = new User('login1', 'qwerty');
-const user2 = new User('superman', 'qwerty12345');
-const user3 = new User('vasya-petrov', 'qwerty');
+let obj1 = addTwoNumbers(5,12);
+// obj1.number1 = 5, obj1.number2 = 12
+// let obj1 = {number1: 5, number2: 12};
 
-console.log('user#1: ',user1);
-console.log('user#2: ',user2);
-console.log('user#3: ',user3);
+let res1 = obj1.add();  // {number1: 5, number2: 12, sum: 17}
 
-console.log('*****************');
 
-user1.outObject();
-user2.outObject();
-user3.outObject();
+***************************
 
-//  класс машин
-// модель, цвет, год выпуска, объект двигателя (модель, мощность)
-
-class Engine{
-    constructor(name, power){
-        this.name = name;
-        this.power = power;
-    }
+class Calc{
+   constructor(number1, number2){
+       this.number1 = number1;
+       this.number2 = number2;
+       this.result = 0;
+   }
+   add(){ this.result  = this.number1 + this.number2; } 
+   sub(){ this.result  = this.number1 - this.number2;}
+   divi(){ this.result  = this.number1 / this.number2; } 
+   mult(){ this.result  = this.number1 * this.number2; }
+   answer() { console.log('Answer is ', this.result);}    
 };
 
-class Car{
-    constructor(model, color, year, engine){
-        this.model = model;
-        this.color = color;
-        this.year = year;
-        this.engine = engine;
-    }
-};
-const engine1 = new Engine('1234-12345677', 1000);
-const car1 = new Car('modelX', 'black', 2022, engine1);
-console.log(car1);
-console.log(car1.model);
-console.log(car1.engine);
+let test = new Calc(10,20);
+test.add();
+test.answer();
+
+
+
+
+
+
+

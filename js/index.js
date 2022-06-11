5574fe6dbb0f5383c1ab36bb71b2271cfada6a9f
pdcourses <freshitpt@gmail.com>
2022-06-11 13:56:57 +0300

    Errors js

--- js/index.js
+++ js/index.js
@@ -1,105 +1,88 @@
-// parent class
-class Animal {
-    constructor(name) {
-      this.speed = 0;
-      this.name = name;
-    }
-    run(speed) {
-      this.speed = speed;
-      console.log(`${this.name} бежит со скоростью ${this.speed}.`);
-    }
-    stop() {
-      this.speed = 0;
-      console.log(`${this.name} стоит.`);
-    }
-  }
-  // child class узконаправленная
- class Rabbit extends Animal {
-    constructor(name) {
-      super(name);
-      this.name = name;
-    }
-    hide() {
-      console.log(`${this.name} прячется!`);
-    }
-  };
+/*
+try{
+    COLOR;
+    // your  code
+} catch(error){
+    console.error('Your error: ',error);
+    //console.log(error.name);
+    //console.log(error.message);
+}
+console.log('hello');
 
-  let Rex = new Animal("Rex");
-  Rex.run(10);
-  Rex.stop();
-
-  let krolik = new Rabbit('Krolik');
-  krolik.hide();  // личный метод свой
-  krolik.run(2); // возможность от родителя
-  krolik.stop()  // возможность от родителя
+*/
+/*
+COLOR;
+// your  code
+console.log('hello');
+*/
 
 
-  /*
-  User - класс родитель login, password
-  Buyer - класс ребенок (наследник) tel, address
-  Worker - дочерний класс  exp, salary, category
-  */ 
-
-  class User {
-    constructor(login, password){
-        this.login = login;
-        this.password = password;
+function divTwoNumber(a,b){
+    try{
+        if(b == 0){
+            throw new Error('Second number must be not zero');
+        }
+    } catch(e){
+        return 0;
     }
-    updatePassword(value){
-        this.password = value;
-    }
-  }
-
-  class Buyer extends User {
-    constructor(login, password, telephone, address) {
-      super(login, password); // contructor User
-      this._telephone = telephone;
-      this._address = address;
-    }
-    set telephone(value){
-        this._telephone = value;
-    }
-    get telephone(){
-        return this._telephone;
-    }
-    set address(value){
-        this._address = value;
-    }
-    get address(){
-        return this._address;
-    }
-    diliAddress() {
-        return this.address+','+this.telephone;
-    }
-  }
-
-  class Worker extends User{
-    constructor(login, password, exp, salary, category){
-        super(login, password);
-        this.exp=exp;
-        this.salary=salary;
-        this.category=category;
-    }
-    infoWorder(){
-        console.log(this.category, ':', this.salary);
-    }
+    return a/b;
 }
 
-const worker1 = new Worker('workerlogin', '12345', 5, 1000, 'category1');
-worker1.infoWorder();
-worker1.updatePassword('admin');
-console.log(worker1);
+console.log(divTwoNumber(20,10));
 
-const buyer1 = new Buyer('buylogin', '12345', 'NY av New 64/15', '0951234578');
-console.log(buyer1);
-console.log(buyer1.diliAddress());
+console.log(divTwoNumber(-120,10));
 
+console.log(divTwoNumber(20,0));
+
+console.log('Hello');
 
 /*
-User - login, pswd
-Worder - 
-Student , 
+class RangeValidator
+fileds : from , to
+methods: 
+1. range - вернет массив стартовый limit и конечный limit  [5,12]
+2. validate - проверяет входит ли число в єтот диапазон
 
+ RangeValidator(5,12);
+ validate(8)
+
+
+ RangeValidator(12,5);   --- > RangeValidator(5,12);
 
 */
 
+
+class RangeValidator {
+    constructor(from, to) {
+        if(Number.isNaN(from) || !Number.isFinite(from)){
+            throw new Error('Error. Number TO is wrong');
+        }
+        if(Number.isNaN(to) || !Number.isFinite(to)){
+            throw new Error('Error. Number FROM is wrong');
+        }
+        this.from = from;
+        this.to = to;
+    }
+    range() {
+        if(this.from > this.to) {
+            let copy_from = from;
+            this.from = this.to;
+            this.to = copy_from;
+        }
+        return [this.from. this.to];
+    }
+    validate(value){
+        if(value >= this.from && value <= this.to) return true;
+        return false;
+    }
+}
+
+
+
+function f(value){
+    if(!Number.isNaN(value) && Number.isFinite(value) && value != 0){
+        // some code
+    } else {
+        throw new Error('Wrong value');
+    }
+}

dafd849e1653797080a8d04ec4bd11e8cfe2a596
pdcourses <freshitpt@gmail.com>
2022-06-11 12:44:36 +0300

    class extends

--- js/index.js
+++ js/index.js
@@ -1,80 +1,105 @@
-/*
-class User {
+// parent class
+class Animal {
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+    run(speed) {
+      this.speed = speed;
+      console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+    }
+    stop() {
+      this.speed = 0;
+      console.log(`${this.name} стоит.`);
+    }
+  }
+  // child class узконаправленная
+ class Rabbit extends Animal {
+    constructor(name) {
+      super(name);
+      this.name = name;
+    }
+    hide() {
+      console.log(`${this.name} прячется!`);
+    }
+  };
+
+  let Rex = new Animal("Rex");
+  Rex.run(10);
+  Rex.stop();
+
+  let krolik = new Rabbit('Krolik');
+  krolik.hide();  // личный метод свой
+  krolik.run(2); // возможность от родителя
+  krolik.stop()  // возможность от родителя
+
+
+  /*
+  User - класс родитель login, password
+  Buyer - класс ребенок (наследник) tel, address
+  Worker - дочерний класс  exp, salary, category
+  */ 
+
+  class User {
     constructor(login, password){
         this.login = login;
         this.password = password;
     }
-    test(){
-        console.log(this.login, ': ', this.password);
-    }
-    updatePassword(newPassword){
-        this.password = newPassword;
-    }
-};
-
-const obj1 = new User('vasya', 'qwerty');
-obj1.test();
-obj1.updatePassword('123dkrke1!-');
-obj1.test();
-*/
-
-// Обновить поле или прочитать значение свойства
-// get - получить , set - установить или обновить
-/*
-class User {
-    constructor(login, password){
-        this.login = login;
-        this.password = password;
-    }
-    test(){
-        console.log(this.login, ': ', this.password);
-    }
-    set password(value){
+    updatePassword(value){
         this.password = value;
     }
-    set login(value){
-        this.login = value;
+  }
+
+  class Buyer extends User {
+    constructor(login, password, telephone, address) {
+      super(login, password); // contructor User
+      this._telephone = telephone;
+      this._address = address;
     }
-    get password(){
-        return this.password;
+    set telephone(value){
+        this._telephone = value;
     }
-    get login(){
-        return this.login;
+    get telephone(){
+        return this._telephone;
     }
-};
+    set address(value){
+        this._address = value;
+    }
+    get address(){
+        return this._address;
+    }
+    diliAddress() {
+        return this.address+','+this.telephone;
+    }
+  }
+
+  class Worker extends User{
+    constructor(login, password, exp, salary, category){
+        super(login, password);
+        this.exp=exp;
+        this.salary=salary;
+        this.category=category;
+    }
+    infoWorder(){
+        console.log(this.category, ':', this.salary);
+    }
+}
+
+const worker1 = new Worker('workerlogin', '12345', 5, 1000, 'category1');
+worker1.infoWorder();
+worker1.updatePassword('admin');
+console.log(worker1);
+
+const buyer1 = new Buyer('buylogin', '12345', 'NY av New 64/15', '0951234578');
+console.log(buyer1);
+console.log(buyer1.diliAddress());
+
+
+/*
+User - login, pswd
+Worder - 
+Student , 
+
+
 */
-//id login pasword fullname ...
-// 1001 'login1' 'qwertt' ........ 
-// 1002 'vasya' 'qwertt' ........
-
-
-
-
-class User {
-    constructor(login, password, fullname = 'uknown'){
-        this.fullname = fullname;
-        this._login = login;
-        this._password = password;
-    }
-    test(){
-        console.log(this._login, ': ', this._password);
-    }
-    set password(password){
-        this._password = password;
-    }
-    set login(login){
-        this._login = login;
-    }
-    get password(){
-        return this._password;
-    }
-    get login(){
-        return this._login;
-    }
-};
-
-let obj2 = new User('vasyalogin123', 'qwerty'); 
-obj2.password = 'admin'; 
-console.log(obj2.password);
-obj2.test();
 

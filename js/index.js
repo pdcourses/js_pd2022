0af2d58a4e4bf7205dfefd558e2ff66cbce2a0e3
pdcourses <freshitpt@gmail.com>
2022-06-12 10:20:16 +0300

    class examples

--- js/index.js
+++ js/index.js
@@ -1,88 +1,71 @@
+// DB
+// client
+// server
+
+
+class genArray{
+    constructor(start, end, step = 1){
+        this.start = start;
+        this.end = end;
+        this.step = step;
+        this.array = [];
+        this._count = (this.end - this.start) / this.step;
+    }
+    makeArray(){
+        this.array[0] = this.start;
+        for(let i=1; i <= this._count; i++){
+            this.array[i] = this.array[i-1] + this.step;
+        }
+        return this.array;
+    }
+    makeArray2(){
+        for(let i=0, value=this.start; value <= this.end; i++, value += this.step){
+            this.array[i] = value;
+        }
+        return this.array;
+    }
+    outArray(){
+        console.log(this.array);
+    }
+
+}
+
+let a1 = new genArray(100, 120, 2);
+a1.makeArray();
+a1.outArray();
+let a2 = new genArray(100, 120);
+a2.makeArray();
+a2.outArray();
 /*
-try{
-    COLOR;
-    // your  code
-} catch(error){
-    console.error('Your error: ',error);
-    //console.log(error.name);
-    //console.log(error.message);
+class ParentClass {
+    constructor(name1, name2 = "unknown"){
+        this.name1 = name1;
+        this.name2 = name2;
+    }
 }
-console.log('hello');
 
+class MyClass extends ParentClass{
+    // this
+    constructor(name1, name2, value1=0, value2=0){
+        super(name1); // constructor parent  
+        // super(name1, name2)
+        this.value1 = value1;
+        this.value2 = value2;
+        this.sum = 0;
+        this._count = 0; // private  only for class
+    }
+    set count(newValue){
+        this._count = newValue;
+    }
+    get count(){
+        return this._count;
+    }
+}
+
+let obj1 = new MyClass(10,20);
+let obj2 = new MyClass(10);
+let obj3 = new MyClass();
+
+console.log(obj3.sum);
+console.log(obj3._count); 
 */
-/*
-COLOR;
-// your  code
-console.log('hello');
-*/
-
-
-function divTwoNumber(a,b){
-    try{
-        if(b == 0){
-            throw new Error('Second number must be not zero');
-        }
-    } catch(e){
-        return 0;
-    }
-    return a/b;
-}
-
-console.log(divTwoNumber(20,10));
-
-console.log(divTwoNumber(-120,10));
-
-console.log(divTwoNumber(20,0));
-
-console.log('Hello');
-
-/*
-class RangeValidator
-fileds : from , to
-methods: 
-1. range - вернет массив стартовый limit и конечный limit  [5,12]
-2. validate - проверяет входит ли число в єтот диапазон
-
- RangeValidator(5,12);
- validate(8)
-
-
- RangeValidator(12,5);   --- > RangeValidator(5,12);
-
-*/
-
-
-class RangeValidator {
-    constructor(from, to) {
-        if(Number.isNaN(from) || !Number.isFinite(from)){
-            throw new Error('Error. Number TO is wrong');
-        }
-        if(Number.isNaN(to) || !Number.isFinite(to)){
-            throw new Error('Error. Number FROM is wrong');
-        }
-        this.from = from;
-        this.to = to;
-    }
-    range() {
-        if(this.from > this.to) {
-            let copy_from = from;
-            this.from = this.to;
-            this.to = copy_from;
-        }
-        return [this.from. this.to];
-    }
-    validate(value){
-        if(value >= this.from && value <= this.to) return true;
-        return false;
-    }
-}
-
-
-
-function f(value){
-    if(!Number.isNaN(value) && Number.isFinite(value) && value != 0){
-        // some code
-    } else {
-        throw new Error('Wrong value');
-    }
-}

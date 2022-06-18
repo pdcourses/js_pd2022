ab862fbf909d95f895102e0a780d0d64faa6dcc3
pdcourses <freshitpt@gmail.com>
2022-06-18 12:21:56 +0300

    user card with function style

--- js/index.js
+++ js/index.js
@@ -1,3 +1,52 @@
+function createUserImage(userImageSrc){
+    const imgCont = document.createElement('div');
+    const imgUser = document.createElement('img');
+    imgUser.setAttribute('src', userImageSrc);
+    imgUser.setAttribute('alt', 'photo');
+    imgCont.append(imgUser);
+    return imgCont;
+}
+
+function createUserFullname(fullName){
+    const userFullName = document.createElement('p');
+    userFullName.classList.add('user_fullname');
+    userFullName.innerText = fullName;
+    return userFullName;
+}
+
+function createBtnContainer(stats){
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('button_container');
+    //fb
+    const stat1 = document.createElement('div');
+    stat1.classList.add('stat', 'fb');
+    stat1.innerText = stats.fb;
+    btnContainer.append(stat1);
+    //insta
+    const stat2 = document.createElement('div');
+    stat2.classList.add('stat', 'insta');
+    stat2.innerText = stats.insta;
+    btnContainer.append(stat2);
+    // tel
+    const stat3 = document.createElement('div');
+    stat3.classList.add('tel', 'fb');
+    stat3.innerText = stats.tel;
+    btnContainer.append(stat3);
+    return btnContainer;
+}
+
+function createCard(objUser){
+    const card = document.createElement('div');
+    card.classList.add('card_container');
+    card.append(createUserImage(objUser.imgSrc));
+    const infoCont = document.createElement('div');
+    infoCont.classList.add('info_container');
+    infoCont.append(createUserFullname(objUser.fullName));
+    infoCont.append(createBtnContainer(objUser.stats));
+    card.append(infoCont);
+    return card;
+}
+
 const user = {
     imgSrc: '#',
     fullName: 'Tom Fox',
@@ -8,53 +57,5 @@ const user = {
     }
 };
 
-const card = document.createElement('div');
-card.classList.add('card_container');
-
-// container for user image
-const imgCont = document.createElement('div');
-
-const imgUser = document.createElement('img');
-imgUser.setAttribute('src', user.imgSrc);
-imgUser.setAttribute('alt', 'photo');
-
-imgCont.append(imgUser);
-card.append(imgCont);
-
-// info container
-const infoCont = document.createElement('div');
-infoCont.classList.add('info_container');
-
-const userFullName = document.createElement('p');
-userFullName.classList.add('user_fullname');
-userFullName.innerText = user.fullName;
-infoCont.append(userFullName);
-
-// btn container
-const btnContainer = document.createElement('div');
-btnContainer.classList.add('button_container');
-
-//fb
-const stat1 = document.createElement('div');
-stat1.classList.add('stat', 'fb');
-stat1.innerText = user.stats.fb;
-btnContainer.append(stat1);
-//insta
-const stat2 = document.createElement('div');
-stat2.classList.add('stat', 'insta');
-stat2.innerText = user.stats.insta;
-btnContainer.append(stat2);
-// tel
-const stat3 = document.createElement('div');
-stat3.classList.add('tel', 'fb');
-stat3.innerText = user.stats.tel;
-btnContainer.append(stat3);
-// присоединение btnContainer в инфо блок
-infoCont.append(btnContainer);
-// инфо контейнер добавляем на карту
-card.append(infoCont);
-
-
 const userCards = document.getElementById('userCardsContainer');
-userCards.append(card);
-
+userCards.append(createCard(user));

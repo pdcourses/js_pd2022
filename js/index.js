3b4dcd6deb9c23b65f989bc7003ae59754899cf2
pdcourses <freshitpt@gmail.com>
2022-06-18 12:02:52 +0300

    user card structure

--- js/index.js
+++ js/index.js
@@ -1,27 +1,60 @@
-/*
-Получение элементов
-1 document.getElementById('idElem')
-2document.querySelector(css)  '.myClass'   '#idElem'   'ul > li:last-child' '.cont > .myClass'
-3 document.querySelectorAll(css)   return []
-4 *document.getElementsByTagName()  getElementsByClassName() getElementsByName()
-5 elem.closest(css)
-6 elem.matches(css)
+const user = {
+    imgSrc: '#',
+    fullName: 'Tom Fox',
+    stats: {
+        fb: '10000',
+        insta: '12500',
+        tel: '8600'
+    }
+};
 
-Атрибуты Элемента
-elem.getAttribute(nameAttr)
-elem.setAttribute(nameAttr, valueAttr)
-elem.hasAttribute(nameAttr)
-elem.attributes 
+const card = document.createElement('div');
+card.classList.add('card_container');
 
-Создание document.createElement(tag)
-Удаление elem.remove()
-Копирование / клонирование elem.cloneNode()
+// container for user image
+const imgCont = document.createElement('div');
 
-Вставка/ добавление
-node.append(elem)
-prepend(elem)
-before() / after()
-replaceWith()
-* appendChild(), insertChild(), replaceChild(), removeChild()
+const imgUser = document.createElement('img');
+imgUser.setAttribute('src', user.imgSrc);
+imgUser.setAttribute('alt', 'photo');
 
-*/
+imgCont.append(imgUser);
+card.append(imgCont);
+
+// info container
+const infoCont = document.createElement('div');
+infoCont.classList.add('info_container');
+
+const userFullName = document.createElement('p');
+userFullName.classList.add('user_fullname');
+userFullName.innerText = user.fullName;
+infoCont.append(userFullName);
+
+// btn container
+const btnContainer = document.createElement('div');
+btnContainer.classList.add('button_container');
+
+//fb
+const stat1 = document.createElement('div');
+stat1.classList.add('stat', 'fb');
+stat1.innerText = user.stats.fb;
+btnContainer.append(stat1);
+//insta
+const stat2 = document.createElement('div');
+stat2.classList.add('stat', 'insta');
+stat2.innerText = user.stats.insta;
+btnContainer.append(stat2);
+// tel
+const stat3 = document.createElement('div');
+stat3.classList.add('tel', 'fb');
+stat3.innerText = user.stats.tel;
+btnContainer.append(stat3);
+// присоединение btnContainer в инфо блок
+infoCont.append(btnContainer);
+// инфо контейнер добавляем на карту
+card.append(infoCont);
+
+
+const userCards = document.getElementById('userCardsContainer');
+userCards.append(card);
+

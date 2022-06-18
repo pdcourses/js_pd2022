c2ba061d799baab9a7642ec432b3d5ad9e9bcfc4
pdcourses <freshitpt@gmail.com>
2022-06-18 13:48:44 +0300

    event listener

--- js/index.js
+++ js/index.js
@@ -1,84 +1,29 @@
-function createUserImage(userImageSrc){
-    const imgCont = document.createElement('div');
-    const imgUser = document.createElement('img');
-    imgUser.setAttribute('src', userImageSrc);
-    imgUser.setAttribute('alt', 'photo');
-    imgCont.append(imgUser);
-    return imgCont;
+/* HTMLElement - Element - Node - EventTarget */
+
+const btn = document.getElementById('btn');
+
+let isHello = true;
+
+btn.addEventListener("click", fun1);
+
+function fun1(e) {
+    alert('Hello, User !');
+    //isHello = !isHello; 
+    btn.removeEventListener('click', fun1);
+    //btn.addEventListener('click', fun2);
 }
 
-function createUserFullname(fullName){
-    const userFullName = document.createElement('p');
-    userFullName.classList.add('user_fullname');
-    userFullName.innerText = fullName;
-    return userFullName;
+function fun2(e) {
+    alert('Bye )');
+    //isHello = !isHello;
+    btn.removeEventListener('click', fun2);
+    //btn.addEventListener('click', fun1);
 }
 
-function createBtnContainer(stats){
-    const btnContainer = document.createElement('div');
-    btnContainer.classList.add('button_container');
-    //fb
-    const stat1 = document.createElement('div');
-    stat1.classList.add('stat', 'fb');
-    stat1.innerText = stats.fb;
-    btnContainer.append(stat1);
-    //insta
-    const stat2 = document.createElement('div');
-    stat2.classList.add('stat', 'insta');
-    stat2.innerText = stats.insta;
-    btnContainer.append(stat2);
-    // tel
-    const stat3 = document.createElement('div');
-    stat3.classList.add('tel', 'fb');
-    stat3.innerText = stats.tel;
-    btnContainer.append(stat3);
-    return btnContainer;
-}
-
-function createCard(objUser){
-    const card = document.createElement('li');
-    card.classList.add('card_container');
-    card.append(createUserImage(objUser.imgSrc));
-    const infoCont = document.createElement('div');
-    infoCont.classList.add('info_container');
-    infoCont.append(createUserFullname(objUser.fullName));
-    infoCont.append(createBtnContainer(objUser.stats));
-    card.append(infoCont);
-    return card;
-}
-
-const users = [
-    {
-        imgSrc: '#',
-        fullName: 'Tom Fox',
-        stats: {
-            fb: '10000',
-            insta: '12500',
-            tel: '8600'
-        }
-    },
-    {
-        imgSrc: '#',
-        fullName: 'John Smith',
-        stats: {
-            fb: '10000',
-            insta: '12500',
-            tel: '8600'
-        }
-    },
-    {
-        imgSrc: '#',
-        fullName: 'Ann Fox',
-        stats: {
-            fb: '10000',
-            insta: '12500',
-            tel: '8600'
-        }
-    },
 
 
-];
+/*
+btn.onclick = function(){ console.log('Hello!!')}
 
-const userCards = document.getElementById('userCardsContainer'); //ulu
-users.forEach( u => userCards.append(createCard(u)))
-
+btn.onclick = function(){ console.log('Bye )')}
+*/

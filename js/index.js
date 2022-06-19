1e402631291cd454841dfc2d5ff5da81f68822b2
pdcourses <freshitpt@gmail.com>
2022-06-19 10:33:00 +0300

    task. вывод даты. счетчик

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
-/*
-<div id="userList" class="container" name="cards" isChoose="1">  
-        <p>first</p>
-        <p>second</p>
-        <p>third</p>
-        <p>4-th</p>      
-    </div>
-    */
+const btn = document.querySelector('.container');
+const p = document.getElementById('info');
 
-let elem = document.getElementById('userList');
-console.log(elem);
-console.log(elem.attributes);
-console.log(elem.attributes[0]); // id 
-console.log(elem.attributes[0].value); // userList
-console.log(elem.nodeType);
-console.log(elem.id);
-console.log(elem.childNodes);
-console.log(elem.children);
-console.log(elem.firstChild);
-console.log(elem.lastChild);
+btn.addEventListener('click', handleFun);
+
+let counter = 0;
+
+/*
+// вывести текущую дату
+function handleFun (e) {
+    console.log(e);
+    const date = new Date();
+    console.log(date);
+    p.innerText = date;
+}
+*/
+
+// счетчик
+// вывести текущую дату
+function handleFun (e) {
+    p.innerText = counter;
+    counter++;
+}
+
+let rem = document.getElementById('remBtn');
+rem.addEventListener('click', (e) => {
+    console.log(e);
+    counter = 0;
+});

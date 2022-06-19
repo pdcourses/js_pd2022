abe54a383b2920ed7e9fe5da185d503bfbcfe07f
pdcourses <freshitpt@gmail.com>
2022-06-19 11:30:52 +0300

    color boxes with classes

--- js/index.js
+++ js/index.js
@@ -1,33 +1,20 @@
 const divArray = document.querySelectorAll('.box');
 
-/*
-const handleChangeColor1 = (e) => {
-    //console.log(this);   window
-    //console.log(e);
-}
-*/
-
-function handleChangeColor1 (e) {
-    //console.log(this);
-    //console.log(e);
-    this.style.background = 'black';
+function handleChangeColor1 (e) {  
+    this.classList.remove('color2');  
+    this.classList.add('color1');
+    console.log(this.classList);
     this.removeEventListener('click', handleChangeColor1);
     this.addEventListener('click', handleChangeColor2);
 }
 
 function handleChangeColor2 (e) {
-    this.style.background = 'white';
+    this.classList.remove('color1'); 
+    this.classList.add('color2');
+    console.log(this.classList);
     this.removeEventListener('click', handleChangeColor2);
     this.addEventListener('click', handleChangeColor1);
 
 }
 
 divArray.forEach( el => el.addEventListener('click', handleChangeColor1));
-
-
-
-/*
-for(let i=0; i<divArray.length; i++){
-    divArray[i].addEventListener();
-}
-*/

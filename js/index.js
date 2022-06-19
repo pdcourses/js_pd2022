22c52306c19d69531d513135dd6f0364dc133fc1
pdcourses <freshitpt@gmail.com>
2022-06-19 12:09:11 +0300

    color boxes. remove box

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const divArray = document.querySelectorAll('.box');
 function handleChangeColor1 (e) {  
     this.classList.remove('color2');  
     this.classList.add('color1');
-    console.log(this.classList);
     this.removeEventListener('click', handleChangeColor1);
     this.addEventListener('click', handleChangeColor2);
 }
@@ -11,10 +10,15 @@ function handleChangeColor1 (e) {
 function handleChangeColor2 (e) {
     this.classList.remove('color1'); 
     this.classList.add('color2');
-    console.log(this.classList);
     this.removeEventListener('click', handleChangeColor2);
     this.addEventListener('click', handleChangeColor1);
 
 }
 
-divArray.forEach( el => el.addEventListener('click', handleChangeColor1));
+divArray.forEach( el => {
+    el.addEventListener('click', handleChangeColor1);
+    // el.firstChild.addEventListener
+    el.children[0].addEventListener('click', (event) => el.remove())
+});
+
+

08ed789955c20397536f4adafa915232c58a89a8
pdcourses <freshitpt@gmail.com>
2022-06-26 13:41:19 +0300

    todo easy

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-const btn = document.getElementById("send");
-const userName = document.getElementById("userName");
-const userPassword = document.getElementById("userPassword");
+const list = document.getElementById('tasks');
+const btnAdd = document.getElementById('btnAdd');
+const btnDel = document.getElementById('btnDel');
+const task = document.getElementById('task');
 
+btnAdd.addEventListener('click', addNewTask);
+btnDel.addEventListener('click', deleteCheckTasks);
 
-const regPassword = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#%\+\*;\.])^.{6,12}$/;
-const strRegPswd = regPassword.toString(); // и обрезать начало и конец
-userPassword.setAttribute('pattern', regPassword);
+function addNewTask(event) {
+    const item = document.createElement('li');
+    item.innerText = task.value;
+    item.append(addCheckbox());
+    list.append(item);
+    task.value = "";
+};
 
+function addCheckbox(){
+    const checkElem = document.createElement('input');
+    checkElem.type = 'checkbox';
+    checkElem.classList.add('check');
+    return checkElem;
+}
 
+function deleteCheckTasks(event){
+    const deletedTasks = document.querySelectorAll('input[type="checkbox"]:checked');
+    deletedTasks.forEach(el => el.parentNode.remove());
+}

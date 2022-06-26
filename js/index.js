f3daa5bb394915536af1ecdf7194da88982c0b83
pdcourses <freshitpt@gmail.com>
2022-06-26 14:10:58 +0300

    slider was added

--- js/index.js
+++ js/index.js
@@ -1,27 +1,18 @@
-const list = document.getElementById('tasks');
-const btnAdd = document.getElementById('btnAdd');
-const btnDel = document.getElementById('btnDel');
-const task = document.getElementById('task');
+const COLORS = ["red", "yellow", "geen", "blue", "orange"];
+const menuContainer = document.querySelector('.container');
+const items = document.querySelectorAll('.item');
+let activeElement = menuContainer.querySelector(".active");
 
-btnAdd.addEventListener('click', addNewTask);
-btnDel.addEventListener('click', deleteCheckTasks);
-
-function addNewTask(event) {
-    const item = document.createElement('li');
-    item.innerText = task.value;
-    item.append(addCheckbox());
-    list.append(item);
-    task.value = "";
-};
-
-function addCheckbox(){
-    const checkElem = document.createElement('input');
-    checkElem.type = 'checkbox';
-    checkElem.classList.add('check');
-    return checkElem;
+function workActive(i, index){
+    if(activeElement == i) return;
+        if(activeElement) activeElement.classList.remove("active");
+        i.classList.add("active");
+        document.body.style.backgroundColor = COLORS[index];
+        activeElement = i;
+        console.log(i,index,activeElement);
 }
 
-function deleteCheckTasks(event){
-    const deletedTasks = document.querySelectorAll('input[type="checkbox"]:checked');
-    deletedTasks.forEach(el => el.parentNode.remove());
-}
+items.forEach((i, index) => {
+    i.addEventListener("click", (event) => workActive(i, index));
+});
+

9b5a0660409e16a8af4eff1d7fa4a65e97cbad92
pdcourses <freshitpt@gmail.com>
2022-06-26 12:53:27 +0300

    valid form

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-<form action="#">
-    
+const btn = document.getElementById("send");
+const userName = document.getElementById("userName");
+const userPassword = document.getElementById("userPassword");
 
-</form>
+
+const regPassword = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#%\+\*;\.])^.{6,12}$/;
+const strRegPswd = regPassword.toString(); // и обрезать начало и конец
+userPassword.setAttribute('pattern', regPassword);
+
+

d48fc8e3f25a8d9c878162d964a5d50fbc3ee8a5
pdcourses <freshitpt@gmail.com>
2022-07-02 10:33:23 +0300

    prototype array

--- js/index.js
+++ js/index.js
@@ -1,21 +1,17 @@
-const COLORS = ["#e91c1c", "#e91c86", "#1eca83", "#561ce9", "#f1a50d"];
-const menuContainer = document.querySelector('.container');
-const items = document.querySelectorAll('.item');
-let indexActive = 0;
-let activeElement = menuContainer[indexActive];
-document.body.style.backgroundColor = COLORS[indexActive];
-
-function workActive(i, index){
-    if(activeElement == i) return;
-        if(activeElement) activeElement.classList.remove("active");
-        i.classList.add("active");
-        document.body.style.backgroundColor = COLORS[index];
-        activeElement = i;
-        console.log(i,index,activeElement);
+function MyArray(){
+    this.length = 0;
 }
+MyArray.prototype = {
+    push(val){
+        this[ this.length++] = val;
+    }
+};
 
-items.forEach((i, index) => {
-    i.style.backgroundColor = COLORS[index];
-    i.addEventListener("click", (event) => workActive(i, index));
-});
+const a = new MyArray();
+console.log(a);
+a.push(10);
+a.push('hello');
+a.push({name: "Vasya"})
+a.push( (() => {} ));
 
+console.log(a);

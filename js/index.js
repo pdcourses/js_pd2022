1e124e76f2c8e3f867297a35ac60a21991bc3b02
pdcourses <freshitpt@gmail.com>
2022-07-02 11:01:18 +0300

    proto arr method pop

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ function MyArray(){
 MyArray.prototype = {
     push(val){
         this[ this.length++] = val;
+    },
+    pop(){
+        if(this.length > 0){
+            const res = this[--this.length];
+            delete this[this.length];
+            return res;
+        }
     }
 };
 
@@ -14,4 +21,7 @@ a.push('hello');
 a.push({name: "Vasya"})
 a.push( (() => {} ));
 
+console.log(a);
+
+a.pop();
 console.log(a);

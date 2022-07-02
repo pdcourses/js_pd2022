b5ecf28517b7469191f4043111f095217f462f43
pdcourses <freshitpt@gmail.com>
2022-07-02 12:17:55 +0300

    structure list

--- js/index.js
+++ js/index.js
@@ -1,27 +1,57 @@
-/*
-class User{
-    constructor(login, password){
-        this.login = login;
-        this.password = password;
+class Elem{
+    constructor(value, next = null, prev = null){
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+class List{
+    constructor(){
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
     }
 
+    addStartElem(value){
+        const newHead = new Elem(value, this.head, null );
+        this.size ++;
+        this.head = newHead;
+        if(!this.tail) { this.tail = newHead; }
+        if(this.head) {this.head.prev = newHead;}
+    }
+    addEndElem(value){
+        const newTail = new Elem(value, null, this.tail);
+        this.size++;
+        this.tail = newTail;
+        if(!this.head) {this.head = newTail;}
+        if(this.tail) {this.tail.next = newTail;}
+
+    }
+    addAfterNode(indexNode, value){
+        if(this.tail == indexNode) {
+            this.addEndElem(value);
+            return;
+        }
+        const newElem = new Elem(value, indexNode.next, indexNode);
+        newElem.next = indexNode;
+        //newElem.next.prev = indexNode;
+        this.size ++;
+
+    }
+    /*
+    addBeforeNode(indexNode, value){
+        if(this.head == indexNode) {
+            this.addStartElem(value);
+            return;
+        }
+        const newElem = new Elem(value, indexNode.next, indexNode);
+        this.size ++;
+        newElem.next = indexNode;
+        newElem.next.prev = indexNode;
+    }*/
 }
-const user1 = new User('vasya10', 'qwerty');
-*/
 
-function User(login, password){
-    this.login = login;
-    this.password = password;
-}
-
-function Stud(name, surname, age){
-    this.name = name;
-    this.surname = surname;
-    this.age = age;
-}
-
-const user1 = new User('vasya10', 'qwerty');
-const stud1 = new Stud('Vasya', 'Ivanov', 20);
-
-Stud.prototype = new User();
-
+const list1 = new List();
+list1.addEndElem(10);
+list1.addEndElem('hello');
+console.log(list1);

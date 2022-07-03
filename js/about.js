eb3f9692e2935c0399478b46ab4cf52fbcbc9ee2
pdcourses <freshitpt@gmail.com>
2022-07-03 14:08:02 +0300

    location. timer

--- js/about.js
+++ js/about.js
@@ -3,3 +3,24 @@ const btnHome = document.getElementById('btnHome');
 btnHome.addEventListener('click', () => {
     location.replace(`${location.origin}`);
 });
+
+
+function funHello(){
+    alert('You are in About page!');
+}
+
+//setTimeout(funHello, 3000);
+
+// менять время
+const counter = document.getElementById('counter');
+let c = 10;
+
+function updateCounter(){
+    if(c>=0){
+    counter.innerText = c;
+    c--;
+    }
+}
+
+setInterval(updateCounter, 1000);
+
--- js/login.js
+++ js/login.js
@@ -15,3 +15,4 @@ data.onsubmit = () => {
     location.replace(location.origin);
     return false;
 };
+
